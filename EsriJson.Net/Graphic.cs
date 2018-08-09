b41c16652ba818ba62e7c61abffe5a1084f012e2
steveoh <sgourley@utah.gov>
2018-08-09 09:09:51 -0600

    refactor: move graphic out of folder

--- EsriJson.Net/Graphic.cs
+++ EsriJson.Net/Graphic.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using EsriJson.Net.Geometry;
 using Newtonsoft.Json;
 
-namespace EsriJson.Net.Graphic
+namespace EsriJson.Net
 {
     public class Graphic : IGeometry
     {
